a36cfbc00709185e89bf95231b756898c26a0991
hsnsmii <hasan7eksi@gmail.com>
2025-06-19 15:22:30 +0300

    sdf
    fsds

--- services/config.js
+++ services/config.js
@@ -1,4 +1,4 @@
 import { API_BASE_URL as ENV_API_BASE_URL, ML_BASE_URL as ENV_ML_BASE_URL } from '@env';
 
-export const API_BASE_URL = ENV_API_BASE_URL || 'http://192.168.1.23:3000';
-export const ML_BASE_URL = ENV_ML_BASE_URL || 'http://192.168.1.23:5050';
+export const API_BASE_URL = ENV_API_BASE_URL || 'http://10.10.197.47:3000';
+export const ML_BASE_URL = ENV_ML_BASE_URL || 'http://10.10.197.47:5050';
